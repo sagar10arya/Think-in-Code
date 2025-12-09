96ccbff21da585c3a69d76f1babc23cb3c66b8b5
Sagar Arya <71361360+sagar10arya@users.noreply.github.com>
2025-12-09 21:14:28 +0530

    Time: 891 ms (47.44%) | Memory: 428.1 MB (25.55%) - LeetSync

--- 3885-count-special-triplets/count-special-triplets.cpp
+++ 3885-count-special-triplets/count-special-triplets.cpp
@@ -1,23 +1,35 @@
 class Solution {
 public:
+    // Time -> O(2N), Space -> O(N)
     int specialTriplets(vector<int>& nums) {
-        int M = 1e9+7;
-        unordered_map<int, int> valid_i;
-        unordered_map<int, int> valid_j;
+        int M = 1e9+7;  // modulo value because the answer can be large
         int count = 0;
+
+        // leftMap[x]  = how many times value x has appeared before current i
+        unordered_map<int, int> leftMap;
+        // rightMap[x] = how many times value x appears after current i
+        unordered_map<int, int> rightMap; 
+
+        // 1st fill rightMap with full frequency
+        for(int num : nums)
+        {
+            rightMap[num]++;
+        }
 
         for(int num : nums)
         {
-            if(num%2==0){
-                // if its valid k, we got our triplets
-                count = (count + valid_j[num/2]) % M;
-            }
+            rightMap[num]--; // remove one occurrence of num from the right side
 
-            // if its valid j or not
-            valid_j[num] = (valid_j[num] + valid_i[num*2]) % M;
+            int left = leftMap[num*2];
+            int right = rightMap[num*2];
 
-            valid_i[num]++;
+            // All pairs (i,j,k) = leftChoices * rightChoices
+            // Multiply using 1LL to prevent integer overflow
+            count = (count + (1LL * left * right)) % M;
+
+            leftMap[num]++;
         }
+
         return count;
     }
 };
