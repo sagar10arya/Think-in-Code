67fa724860a401d7f67ac3642d9fead58ce791b2
Sagar Arya <71361360+sagar10arya@users.noreply.github.com>
2025-12-25 09:32:36 +0530

    Time: 0 ms (100.00%) | Memory: 26.3 MB (33.31%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -3,11 +3,23 @@ public:
     // TC: O(log n)
     int singleNonDuplicate(vector<int>& nums) {
         int n = nums.size();
-        int ans = 0;
-        for(int i=0; i<n; i++)
+        if(n == 1) return nums[0];  // Edge case: only one element
+        if(nums[0] != nums[1]) return nums[0];  // Edge case: unique element at the beginning
+        if(nums[n-1] != nums[n-2]) return nums[n-1];    // Edge case: unique element at the en
+        int low = 1, high = n-2;
+        while(low <= high)
         {
-            ans = ans ^ nums[i];
+            int mid = low + (high - low)/ 2;
+            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]) return nums[mid];
+
+            // we are on the left and unique element is in the right
+            if((mid%2==1 && nums[mid] == nums[mid-1]) || 
+                (mid%2==0 && nums[mid] == nums[mid+1])){
+                low = mid + 1;
+            }
+            // we are on the right and unique element is on the left
+            else high = mid-1;
         }
-        return ans;
+        return -1;
     }
 };
