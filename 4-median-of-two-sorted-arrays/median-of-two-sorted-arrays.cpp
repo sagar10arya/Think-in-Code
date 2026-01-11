ad5e0886aeccec974333fdbbd73131c8e3326bd8
Sagar Arya <71361360+sagar10arya@users.noreply.github.com>
2026-01-11 18:55:17 +0530

    Time: 0 ms (100.00%) | Memory: 95 MB (93.48%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,25 +1,41 @@
 class Solution {
 public:
-    // brute force
-    // time and space : O(n + m)
+    // time : O(n1 + n2), space: O(1)
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int i=0, j=0;
-        int n = nums1.size(), m = nums2.size();
-        vector<int> nums;
-        while(i<n && j<m)
+        int n1 = nums1.size(), n2 = nums2.size();
+        int n = n1 + n2;
+        int ind2 = n/2, ind1 =ind2 - 1; // Median indices (0-based)
+        int cnt = 0;
+        int ind1ele = INT_MIN, ind2ele = INT_MIN; // To store median elements
+
+        while(i<n1 && j<n2)
         {
-            if(nums1[i] < nums2[j]) nums.push_back(nums1[i++]);
-            else nums.push_back(nums2[j++]);
+            if(nums1[i] < nums2[j]){
+                if(cnt == ind1) ind1ele = nums1[i];
+                if(cnt == ind2) ind2ele = nums1[i];
+                cnt++; i++;
+            } else {
+                if(cnt == ind1) ind1ele = nums2[j];
+                if(cnt == ind2) ind2ele = nums2[j];
+                cnt++; j++;
+            }
         }
-        while(i<n) nums.push_back(nums1[i++]);
-        while(j<m) nums.push_back(nums2[j++]);
-        
-        int x = nums.size(); // size of merged array
-        if(x % 2 == 0){
-            return (nums[x/2] + nums[(x/2)-1]) / 2.0;
-        } else{
-            return nums[x/2];
+
+        while(i<n1){
+            if(cnt == ind1) ind1ele = nums1[i];
+            if(cnt == ind2) ind2ele = nums1[i];
+            cnt++; i++;
         }
-        return 0;
+
+        while(j<n2){
+            if(cnt == ind1) ind1ele = nums2[j];
+            if(cnt == ind2) ind2ele = nums2[j];
+            cnt++; j++;
+        }
+
+        if(n%2 == 1) return ind2ele;
+
+        return (double)(ind1ele + ind2ele) / 2.0;
     }
 };
