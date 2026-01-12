955df3e406cd5f631cad3ed3d5ae7fe6ed2c66f2
Sagar Arya <71361360+sagar10arya@users.noreply.github.com>
2026-01-12 20:44:36 +0530

    Time: 0 ms (100.00%) | Memory: 95 MB (93.50%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,41 +1,38 @@
 class Solution {
 public:
-    // time : O(n1 + n2), space: O(1)
+    // time : O(log(min(n1, n2))), space: O(1)
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int i=0, j=0;
         int n1 = nums1.size(), n2 = nums2.size();
+
+        // Always binary search on smaller array
+        if(n1 > n2) return findMedianSortedArrays(nums2, nums1);
+        // if (nums1.size() > nums2.size()) swap(nums1, nums2);   // ensure nums1 is smaller
+
+
+        int low = 0, high = n1;
+        int left = (n1 + n2 + 1) / 2;   // no. of elements on the left side
         int n = n1 + n2;
-        int ind2 = n/2, ind1 =ind2 - 1; // Median indices (0-based)
-        int cnt = 0;
-        int ind1ele = INT_MIN, ind2ele = INT_MIN; // To store median elements
-
-        while(i<n1 && j<n2)
+        while(low <= high)
         {
-            if(nums1[i] < nums2[j]){
-                if(cnt == ind1) ind1ele = nums1[i];
-                if(cnt == ind2) ind2ele = nums1[i];
-                cnt++; i++;
-            } else {
-                if(cnt == ind1) ind1ele = nums2[j];
-                if(cnt == ind2) ind2ele = nums2[j];
-                cnt++; j++;
+            int mid1 = low + (high - low) / 2;
+            int mid2 = left - mid1;
+            // Boundary values
+            int l1 = INT_MIN, l2 = INT_MIN;
+            int r1 = INT_MAX, r2 = INT_MAX;
+            // Right elements
+            if(mid1 < n1) r1 = nums1[mid1];
+            if(mid2 < n2) r2 = nums2[mid2];
+            // Left elements
+            if(mid1 - 1 >= 0) l1 = nums1[mid1-1];
+            if(mid2 - 1 >= 0) l2 = nums2[mid2-1];
+            if(l1 <= r2 && l2 <= r1)
+            {
+                if(n % 2 == 1) return max(l1, l2); // odd
+                return (double) (max(l1, l2) + min(r1, r2)) / 2.0; // even
             }
+            else if(l1 > r2) high = mid1 - 1;
+            else low = mid1 + 1;
         }
-
-        while(i<n1){
-            if(cnt == ind1) ind1ele = nums1[i];
-            if(cnt == ind2) ind2ele = nums1[i];
-            cnt++; i++;
-        }
-
-        while(j<n2){
-            if(cnt == ind1) ind1ele = nums2[j];
-            if(cnt == ind2) ind2ele = nums2[j];
-            cnt++; j++;
-        }
-
-        if(n%2 == 1) return ind2ele;
-
-        return (double)(ind1ele + ind2ele) / 2.0;
+        return 0;
     }
 };
