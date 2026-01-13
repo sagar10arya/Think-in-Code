87f708c565fd6f650a8b1427387747c58f090079
Sagar Arya <71361360+sagar10arya@users.noreply.github.com>
2026-01-13 20:05:58 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (7.10%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
+    // TC: O(log(m*n))
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n = matrix.size();
         int m = matrix[0].size();
-
-        for(int i=0; i<n; i++)
+        int low = 0, high = n * m - 1;
+        // flattening 2D into virtual 1D array
+        while(low <= high)
         {
-            for(int j=0; j<m ; j++)
-            {
-                if(matrix[i][j] == target) return true;
-            }
+            int mid = low + (high - low) / 2;
+            // convert 1D index to 2D index
+            int row = mid / m;
+            int col = mid % m;
+            if(matrix[row][col] == target) return true;
+            else if(matrix[row][col] < target) low = mid + 1;
+            else high = mid - 1;
         }
         return false;
     }
