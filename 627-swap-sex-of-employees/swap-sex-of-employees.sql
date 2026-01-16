735659130e61b54b9c8d9616920c7da3a6ccedeb
Sagar Arya <71361360+sagar10arya@users.noreply.github.com>
2026-01-16 20:14:21 +0530

    Time: 228 ms (69.13%) | Memory: 0B (100.00%) - LeetSync

--- 627-swap-sex-of-employees/swap-sex-of-employees.sql
+++ 627-swap-sex-of-employees/swap-sex-of-employees.sql
@@ -1,5 +1,9 @@
-update salary
-set sex = CASE
-        when sex = "m" then "f"
-        else "m"
-        end;
+-- update salary
+-- set sex = CASE
+--         when sex = "m" then "f"
+--         else "m"
+--         end;
+
+-- ascii math trick
+UPDATE Salary
+SET sex = CHAR(ASCII('m') + ASCII('f') - ASCII(sex));
