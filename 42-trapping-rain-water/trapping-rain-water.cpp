3357150179622382053eea098adafe7cea697fc6
Sagar Arya <71361360+sagar10arya@users.noreply.github.com>
2026-02-09 20:28:33 +0530

    Time: 0 ms (100.00%) | Memory: 25.7 MB (99.57%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,37 +1,24 @@
 class Solution {
-    vector<int> prefixMaximum(vector<int>& height, int n){
-        vector<int> prefix(n);
-        prefix[0] = height[0];
-        for(int i=1; i<n; i++)
-        {
-            prefix[i] = max(prefix[i-1], height[i]);
-        }
-        return prefix;
-    }
-
-    vector<int> suffixMaximum(vector<int>& height, int n){
-        vector<int> suffix(n);
-        suffix[n-1] = height[n-1];
-        for(int i=n-2; i>=0; i--)
-        {
-            suffix[i] = max(suffix[i+1], height[i]);
-        }
-        return suffix;
-    }
-
 public:
-    // TC: O(3N), SC: O(2N)
+    // TC: O(N), SC: O(1) : Optimal
     int trap(vector<int>& height) {
         int n = height.size();
-        vector<int> prefixMax = prefixMaximum(height, n);
-        vector<int> suffixMax = suffixMaximum(height, n);
+        int leftMax = 0, rightMax = 0;
+        int l = 0, r = n-1;
         int total = 0;
-        for(int i=0; i<n; i++)
+
+        while(l < r)
         {
-            int leftMax = prefixMax[i];
-            int rightMax = suffixMax[i];
-            if(height[i] < leftMax && height[i] < rightMax){
-                total += min(leftMax, rightMax) - height[i];
+            leftMax = max(leftMax, height[l]);
+            rightMax = max(rightMax, height[r]);
+
+            if(leftMax < rightMax){ // left deciding factor(small)
+                total += leftMax - height[l];
+                l++;
+            }
+            else{ // right deciding factor(small)
+                total += rightMax - height[r];
+                r--;
             }
         }
         return total;
