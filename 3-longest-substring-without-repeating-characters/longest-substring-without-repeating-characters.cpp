4c364454e7a4f550a49f37dad345eb00d2f9b1b6
Sagar Arya <71361360+sagar10arya@users.noreply.github.com>
2026-02-21 21:16:05 +0530

    Time: 3 ms (79.57%) | Memory: 11.8 MB (63.98%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    // Brute: generating all substrings and check unique chars
-    // TC: O(N^2), SC:O(256) ->all ascii chars
+    // TC: O(N), SC:O(256) ->all ascii chars
     int lengthOfLongestSubstring(string s) {
         int n = s.length();
         int maxLen = 0;
+        vector<int> lastIndex(256, -1); // for every ascii char->stores where it was last seen
 
-        for(int i = 0; i < n; i++) {
-            unordered_set<char> st; // new set every time
-
-            for(int j = i; j < n; j++) {
-                if(st.find(s[j]) != st.end()) {
-                    break;
-                }
-                st.insert(s[j]);
-                maxLen = max(maxLen, j - i + 1);
+        int left = 0, right = 0;
+        while(right < n)
+        {   
+            if(lastIndex[s[right]] >= left){
+                left = lastIndex[s[right]] + 1;
             }
+
+            lastIndex[s[right]] = right;
+            maxLen = max(maxLen, right - left + 1);
+            right++;
         }
+
         return maxLen;
     }
 };
