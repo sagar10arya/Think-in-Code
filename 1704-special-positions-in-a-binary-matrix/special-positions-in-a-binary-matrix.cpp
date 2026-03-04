586bcb3c24597584a83f48ac3bc5a23a7c29b94e
Sagar Arya <71361360+sagar10arya@users.noreply.github.com>
2026-03-04 21:57:55 +0530

    Time: 0 ms (100.00%) | Memory: 16.9 MB (57.25%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
-    // Brute
-    // TC: O(n*m*(n+m))
+    // Optimal
+    // Time: O(n*m)
+    // Space: O(n + m)
     int numSpecial(vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
         int cnt = 0;
+
+        vector<int> row(n, 0);
+        vector<int> col(m,0);
+
+        // count ones
         for(int i=0; i<n; i++)
         {
             for(int j = 0; j<m; j++){
-                if(mat[i][j] == 1)
-                {
-                    bool special = true;
-                    // check row
-                    for(int col=0; col<m; col++){
-                        if(col != j && mat[i][col] == 1){
-                            special = false;
-                            break;
-                        }
-                    }
+                if(mat[i][j] == 1){
+                    row[i]++; col[j]++;
+                }
+            }
+        }
 
-                    // check col
-                    for(int row=0; row<n; row++){
-                        if(row != i && mat[row][j] == 1){
-                            special = false;
-                            break;
-                        }
-                    }
-
-                    if(special) cnt++;
+        for(int i=0; i<n; i++)
+        {
+            for(int j = 0; j<m; j++){
+                if(mat[i][j] == 1 && row[i] == 1 && col[j] == 1){
+                    cnt++;
                 }
             }
         }
