0095d9f52388f359dafbb3d139b990954748df99
Sagar Arya <71361360+sagar10arya@users.noreply.github.com>
2026-03-08 22:32:35 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (72.81%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -1,30 +1,22 @@
 class Solution {
-    // Time: O(2^n)
-    // Space: recursion stack
 public:
-    string ans;
-    unordered_set<string> st;
-
-    void solve(string curr, int n){
-        if(curr.size() == n)
-        {
-            if(st.count(curr) == 0){
-                ans = curr;
-            }
-            return;
-        }
-        if(!ans.empty()) return;
-
-        solve(curr + "0", n);
-        solve(curr + "1", n);
-    }
-
+    // Cantor's diagonalization principle
+    // TC  : O(n)
+    // SC : O(1)
     string findDifferentBinaryString(vector<string>& nums) {
         int n = nums.size();
-        for(auto &x: nums){
-            st.insert(x);
+        string ans = "";
+        for(int i=0; i<n; i++)
+        {
+            // Look at the diagonal element nums[i][i]
+            // Flip the bit:
+            // if '0' -> put '1'
+            // if '1' -> put '0'
+            // This ensures our new string differs from nums[i]
+            // ensures the string differs from the i-th string at the i-th position
+            if(nums[i][i] == '0') ans += "1";
+            else ans += "0";
         }
-        solve("", n);
         return ans;
     }
 };
